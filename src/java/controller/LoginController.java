e432cbf5fdb8e363ccea6ab8b1a27387e03ee820
truongngo056 <66856930+truongngo056@users.noreply.github.com>
2026-06-14 18:48:52 +0700

    Update lại login controller

--- src/java/controller/LoginController.java
+++ src/java/controller/LoginController.java
@@ -78,82 +78,43 @@ public class LoginController extends HttpServlet {
             return;
         }
 
-        // check status tài khoản 
+        //tung: 14/6 check status tài khoản 
         String status = account.isStatus();
-        if ("Pending".equalsIgnoreCase(status)) {
-            request.setAttribute("error", "Account is pending...");
-            request.setAttribute("email", email);
-            request.getRequestDispatcher("MainController?action=home").forward(request, response);
+        if ("Rejected".equalsIgnoreCase(status)) {
+            request.getRequestDispatcher("MainController?action=pending_page").forward(request, response);
+        }
+        else if ("Pending".equalsIgnoreCase(status)) {
+            request.getRequestDispatcher("ResubmitRegistController").forward(request, response);
             return;
         }
 
-        // otp: Chặn Frozen
-        // if ("Frozen".equalsIgnoreCase(status)) 
         // update thời gian đăng nhập
         accountDAO.updateLastLogin(account.getAccountID());
 
         // Lưu account vào session
         HttpSession session = request.getSession();
         session.setAttribute("ACCOUNT", account);
-        
+
         // Phân luồng theo role
         int roleID = account.getRoleID();
 
-<<<<<<< Updated upstream
         if (roleID == 1) {
             // Admin
             request.getRequestDispatcher("AdminDashboardController").forward(request, response);
             return;
         }
-=======
-<<<<<<< Updated upstream
-        // THÊM ĐOẠN PHÂN QUYỀN NÀY VÀO:
-        if (account.getRoleID() == 1) {
-            // 1 là Admin
-            request.getRequestDispatcher("admin_dashbroad.jsp").forward(request, response);
-        } else if (account.getRoleID() == 3) {
-            // 3 là Doanh Nghiệp -> Đẩy về trang Business
-            response.sendRedirect("MainController?action=BusinessDashboard");
-        } else {
-            // Mặc định (2) là Khách hàng cá nhân
-            request.getRequestDispatcher("CustomerDashBoardController").forward(request, response);
-        }
-=======
-        if (roleID == 1) {
-            // Admin
-            request.getRequestDispatcher("MainController?action=dashboard").forward(request, response);
-            return;
-        }
->>>>>>> Stashed changes
 
-        // Kiểm tra Business
+        // phan luong cho business da duoc approve
         CustomerDAO customerDAO = new CustomerDAO();
         Customer customer = customerDAO.getCustomerByAccountID(account.getAccountID());
-
-        if (customer != null) {
-            BusinessDAO businessDAO = new BusinessDAO();
-<<<<<<< Updated upstream
-            Business business = businessDAO.getBussinessByID(String.valueOf(customer.getCusID()));
-            if (business != null) {
-                // forward ve trang cua busi
-                request.getRequestDispatcher("BusinessDashboardController").forward(request, response);
-                return;
-            }
+        BusinessDAO businessDAO = new BusinessDAO();
+        Business business = businessDAO.getBussinessByCusID(customer.getCusID());
+        request.getSession().setAttribute("BUS", business);
+        if (business != null) {
+            request.getRequestDispatcher("BusinessDashboardController").forward(request, response);
+            return;
         }
-
         request.getRequestDispatcher("CustomerDashBoardController").forward(request, response);
-=======
-            Business business = businessDAO.getBussinessByID(customer.getCusID());
-            if (business != null) {
-                // forward ve trang cua busi
-                request.getRequestDispatcher("MainController?action=dashboard").forward(request, response);
-                return;
-            } //
-        }
-
-        request.getRequestDispatcher("MainController?action=dashboard").forward(request, response);
->>>>>>> Stashed changes
->>>>>>> Stashed changes
     }
 
     /**
