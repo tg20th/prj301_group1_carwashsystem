398474c19765de28cec53f0b9cefc3bceffe4417
truongngo056 <66856930+truongngo056@users.noreply.github.com>
2026-06-14 18:41:47 +0700

    Bo cmt ham

--- src/java/dao/AccountDAO.java
+++ src/java/dao/AccountDAO.java
@@ -268,83 +268,35 @@ public class AccountDAO {
     }
 
 
-<<<<<<< Updated upstream
-//
-//    public int getTotalPendingAccount() {
-//        int result = 0;
-//        Connection cn = null;
-//
-//        try {
-//            cn = DBUtils.getConnection();
-//            String sql = "SELECT ISNULL(COUNT(*), 0) AS [NumOfPending]\n"
-//                    + "FROM [AutoWashProDB].[dbo].[Accounts] WHERE [Status] = 'Pending'";
-//
-//            PreparedStatement st = cn.prepareStatement(sql);
-//
-//            ResultSet table = st.executeQuery();
-//            while (table.next()) {
-//                result = table.getInt("NumOfPending");
-//            }
-//
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        } finally {
-//            try {
-//                if (cn != null) {
-//                    cn.close();
-//                }
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//            }
-//        }
-//
-//        return result;
-//    }
-=======
-<<<<<<< Updated upstream
-=======
 
-    public int getTotalPendingAccount() {
-        int result = 0;
-        Connection cn = null;
+   public int getTotalPendingAccount() {
+       int result = 0;
+       Connection cn = null;
 
->>>>>>> Stashed changes
-        try {
-            cn = DBUtils.getConnection();
-            String sql = "SELECT ISNULL(COUNT(*), 0) AS [NumOfPending]\n"
-                    + "FROM [AutoWashProDB].[dbo].[Accounts] WHERE [Status] = 'Pending'";
-<<<<<<< Updated upstream
-            
-            PreparedStatement st = cn.prepareStatement(sql);
-            
-            ResultSet table = st.executeQuery();
-            while(table.next()) {
-                result = table.getInt("NumOfPending");
-            }
-            
-=======
+       try {
+           cn = DBUtils.getConnection();
+           String sql = "SELECT ISNULL(COUNT(*), 0) AS [NumOfPending]\n"
+                   + "FROM [AutoWashProDB].[dbo].[Accounts] WHERE [Status] = 'Pending'";
 
-            PreparedStatement st = cn.prepareStatement(sql);
+           PreparedStatement st = cn.prepareStatement(sql);
 
-            ResultSet table = st.executeQuery();
-            while (table.next()) {
-                result = table.getInt("NumOfPending");
-            }
+           ResultSet table = st.executeQuery();
+           while (table.next()) {
+               result = table.getInt("NumOfPending");
+           }
 
->>>>>>> Stashed changes
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (cn != null) {
-                    cn.close();
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
+       } catch (Exception e) {
+           e.printStackTrace();
+       } finally {
+           try {
+               if (cn != null) {
+                   cn.close();
+               }
+           } catch (Exception e) {
+               e.printStackTrace();
+           }
+       }
 
-        return result;
-    }
->>>>>>> Stashed changes
+       return result;
+   }
 }
