a49df69a36102642f495227eb32fde99c47a3e19
truongngo056 <66856930+truongngo056@users.noreply.github.com>
2026-06-19 12:23:05 +0700

    xóa hàm trùng

--- src/java/dao/BookingDAO.java
+++ src/java/dao/BookingDAO.java
@@ -686,39 +686,6 @@ public class BookingDAO {
         return result;
     }
 
-}
-
-public int markNoShowBookings() {
-    int result = 0;
-    Connection cn = null;
-
-    try {
-        cn = DBUtils.getConnection();
-        String sql = "UPDATE b "
-                + "SET b.Status = 'NoShow' "
-                + "FROM Bookings b JOIN TimeSlots t "
-                + "ON b.TimeSlotID = t.TimeSlotID "
-                + "WHERE DATEADD(MINUTE, 15, t.StartTime) <= GETDATE() "
-                + "AND b.Status = 'Confirmed'";
-
-        PreparedStatement st = cn.prepareStatement(sql);
-        result = st.executeUpdate();
-
-    } catch (Exception e) {
-        e.printStackTrace();
-    } finally {
-        try {
-            if (cn != null) {
-                cn.close();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    return result;
-}
-
 private Integer getTimeSlotIdByBooking(int bookingId) {
     Connection cn = null;
     try {
